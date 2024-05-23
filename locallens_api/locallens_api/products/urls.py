d73195596c5a796bb8905c1b29a3e97f11811947
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-23 01:34:26 +0100

    fix: restore urls

--- locallens_api/locallens_api/products/urls.py
+++ locallens_api/locallens_api/products/urls.py
@@ -1,15 +1,13 @@
-"""
+from django.urls import path
+
 from .views import ProductListCreateView
 from .views import ProductRetrieveUpdateDestroyView
-"""
 
 urlpatterns = [
-    """
-    # path("/", ProductListCreateView.as_view(), name="product_list_create"),
-    # path(
-    #     "<int:pk>/",
-    #     ProductRetrieveUpdateDestroyView.as_view(),
-    #     name="product_retrieve_update_destroy",
-    # ),
-    """,
+    path("/", ProductListCreateView.as_view(), name="product_list_create"),
+    path(
+        "<int:pk>/",
+        ProductRetrieveUpdateDestroyView.as_view(),
+        name="product_retrieve_update_destroy",
+    ),
 ]
