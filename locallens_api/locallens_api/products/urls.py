40dae2197cc45c1c420d09e78bbdddb37bcaa6e1
Noureddine hamid <hamid-savana@hotmail.fr>
2024-05-23 01:27:09 +0100

    fix: comment unavailable views

--- locallens_api/locallens_api/products/urls.py
+++ locallens_api/locallens_api/products/urls.py
@@ -1,13 +1,15 @@
-from django.urls import path
-
+"""
 from .views import ProductListCreateView
 from .views import ProductRetrieveUpdateDestroyView
+"""
 
 urlpatterns = [
-    path("/", ProductListCreateView.as_view(), name="product_list_create"),
-    path(
-        "<int:pk>/",
-        ProductRetrieveUpdateDestroyView.as_view(),
-        name="product_retrieve_update_destroy",
-    ),
+    """
+    # path("/", ProductListCreateView.as_view(), name="product_list_create"),
+    # path(
+    #     "<int:pk>/",
+    #     ProductRetrieveUpdateDestroyView.as_view(),
+    #     name="product_retrieve_update_destroy",
+    # ),
+    """,
 ]
